091f9d6a710040125deb5413f0b9845db0625e9c
mrwebwork <tech@allanswebwork.info>
2023-08-15 08:54:57 -0500

    Add height to web page, update metadata and add Button component

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <div>
-        <h1 className="text-6xl text-sky-500">Hello, AI Saas</h1>
-        <Button>Click Me</Button>
-      </div>
+      <h1 className="text-6xl text-sky-500">Hello, AI Saas</h1>
+      <Button variant="destructive" size="lg">
+        Click Me
+      </Button>
     </div>
   );
 }
