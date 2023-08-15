5d2ea34e391d50be5184a7f5acb24c99f3e78918
mrwebwork <tech@allanswebwork.info>
2023-08-15 09:44:13 -0500

    Setup Folder structure for project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-6xl">Dashboard Page (Protected)</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
