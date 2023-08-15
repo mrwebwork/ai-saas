2c56c37dbfa55b34cecb0b75a17f7eba4e6c2e78
mrwebwork <tech@allanswebwork.info>
2023-08-15 16:52:16 -0500

    Add UserButton component and sign out on dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
   return (
     <div>
-      <h1 className="text-6xl">Dashboard Page (Protected)</h1>
+      <h1 className="text-2xl">Dashboard Page (Protected)</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
