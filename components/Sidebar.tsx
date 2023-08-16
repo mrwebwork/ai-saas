8611c8fe2af7c5681f359c49f75437f7cce267b9
mrwebwork <tech@allanswebwork.info>
2023-08-16 07:17:31 -0500

    Add logo image to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+
 const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      <div>Hello Sidebar Component</div>
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <Image fill src="/logo.png" alt="Logo" layout="fill" />
+          </div>
+          <h1>Alchemy</h1>
+        </Link>
+      </div>
     </div>
   );
 };
