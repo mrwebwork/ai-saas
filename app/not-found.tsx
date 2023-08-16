bd0476f02f900563b97950297e938baebf46297d
mrwebwork <tech@allanswebwork.info>
2023-08-16 08:42:35 -0500

    Edit not-found with brand colors and accessibility

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,18 +2,21 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-full bg-gradient-to-r from-slate-800/60 via-slate-400/60 to-slate-500/80 text-center text-black">
-      <h1 className="text-5xl pb-5 font-black tracking-wide">404</h1>
+    <main className="flex flex-col items-center justify-center h-full bg-gradient-to-r from-purple-600/70 via-rose-500/70 to-amber-500/90 text-center text-white">
+      <h1 className="sr-only">Page Not Found</h1>
+      <div className="text-5xl font-black tracking-wide animate-bounce">
+        404
+      </div>
       <h2 className="font-medium text-2xl">Looks Deserted Here!</h2>
       <p className="py-5 font-medium text-lg">
         It seems the page you're looking for is playing hide and seek!
       </p>
       <Link
         href="/dashboard"
-        className="bg-slate-50 px-8 py-1 font-semibold rounded-lg"
+        className="bg-slate-50 hover:bg-slate-50/90 transition px-8 py-1 font-semibold rounded-lg text-black"
       >
         Let’s Head Back
       </Link>
-    </div>
+    </main>
   );
 }
