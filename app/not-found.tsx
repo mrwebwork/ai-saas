f7731160da4f4d27279e7313f0822b5f84bdb7aa
mrwebwork <tech@allanswebwork.info>
2023-08-16 08:00:47 -0500

    Add bg-opacity to 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className="flex flex-col items-center justify-center h-full bg-indigo-500/25">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
