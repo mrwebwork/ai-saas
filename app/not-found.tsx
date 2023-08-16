4b3a77d28f2718a515d86a9a37abf35cf211e0ea
mrwebwork <tech@allanswebwork.info>
2023-08-16 08:20:16 -0500

    Add bg-gradient to not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,18 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-full bg-indigo-500/25">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center h-full bg-gradient-to-r from-slate-800/60 via-slate-400/60 to-slate-500/80 text-center text-black">
+      <h1 className="text-5xl pb-5 font-black tracking-wide">404</h1>
+      <h2 className="font-medium text-2xl">Looks Deserted Here!</h2>
+      <p className="py-5 font-medium text-lg">
+        It seems the page you're looking for is playing hide and seek!
+      </p>
+      <Link
+        href="/dashboard"
+        className="bg-slate-50 px-8 py-1 font-semibold rounded-lg"
+      >
+        Let’s Head Back
+      </Link>
     </div>
   );
 }
