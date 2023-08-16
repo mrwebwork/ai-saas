85b79ec122ea5d0a15fa1e527fad77a0531eb752
mrwebwork <tech@allanswebwork.info>
2023-08-16 07:10:17 -0500

    Create sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div>Hello Sidebar Component</div>
+    </div>
+  );
+};
+
+export default Sidebar;
