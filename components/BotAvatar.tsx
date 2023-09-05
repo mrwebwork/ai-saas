dcfa5fdd4d5e72716573956175e6397cfa98130b
mrwebwork <tech@allanswebwork.info>
2023-09-05 01:03:11 -0500

    Create UserAvatar component and BotAvatar component

---
+++ components/BotAvatar.tsx
@@ -0,0 +1,9 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export const BotAvatar = () => {
+  return (
+    <Avatar className="h-8 w-8">
+      <AvatarImage className="p-1" src="/logo.png" />
+    </Avatar>
+  );
+};
